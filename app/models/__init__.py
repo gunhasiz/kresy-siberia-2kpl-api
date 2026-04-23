783b244cf33dbf87e66f9b18efc89e43661855fb
gunhasiz <koflus@hotmail.com>
2026-04-23 18:40:06 +0200

    Add module __init__ file

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+from app.models.base import BaseDTO
+from app.models.status import StatusDTO
+from app.models.person import PersonDTO
+from app.models.personal_situation_outbreak import PersonalSituationOutbreakOfWWIIDTO
+from app.models.deporation_and_repression import DeportationAndRepressionDTO, PlacesDTO
+from app.models.repatriation import RepatriationDTO
+from app.models.occupation_period import OccupationPeriodDTO
+from app.models.military_experience import MilitaryExperienceDTO, MilitaryServiceDTO
+from app.models.other_military_experience import OtherMilitaryExperienceDTO
+from app.models.sources import SourcesDTO
+from app.models.related_galleries import RelatedGalleriesDTO
